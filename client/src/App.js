53ed0c7d52aec6cda6a5c392be56350634ae1020
JMan <60026263+JMantis0@users.noreply.github.com>
2020-09-20 03:16:49 -0700

    made a home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
 import About from "./components/About";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function App() {
-
-
   return (
     <div className="App">
       <CssBaseline />
       <Router>
-        <Header  />
-        <Route path="/" />
+        <Header />
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route exact path="/portfolio">
           <Portfolio />
         </Route>
