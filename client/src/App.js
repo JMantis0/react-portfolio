fa19f513ee6e3ae6a11a74e159c4fd35f5adb18f
JMan <60026263+JMantis0@users.noreply.github.com>
2020-09-21 13:40:06 -0700

    Implemented <Grid> component, useMediaQuery, and useTheme hooks from material-ui library.  Applied media query to highest root grid to control margins above and below 600px.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
 import About from "./components/About";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
+  const theme = useTheme();
+  //  This is a media query hook for React from the material-ui library
+  const aboveSmall = useMediaQuery(theme.breakpoints.up("sm"));
+  const useStyles = makeStyles({
+    root: {
+      //  Control margins based on useMediaQuery hooks
+      width: aboveSmall ? "90%" : "100%",
+      marginLeft: aboveSmall ? "5%" : "0%",
+      backgroundColor: "eeff00",
+    },
+  });
+  const classes = useStyles();
   return (
-    <div className="App">
+    <Grid className={classes.root} container>
       <CssBaseline />
       <Router>
         <Header />
@@ -21,7 +37,7 @@ function App() {
           <About />
         </Route>
       </Router>
-    </div>
+    </Grid>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Grid from "@material-ui/core/Grid"
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,7 +13,7 @@ import {
   Route,
   useLocation,
   Redirect,
-  useHistory
+  useHistory,
 } from "react-router-dom";
 
 const useStyles = makeStyles({
@@ -21,6 +22,10 @@ const useStyles = makeStyles({
   },
   inactiveTab: {
     color: "gray",
+  },
+  appBar: {
+    backgroundColor: "#92A8A8",
+    padding: "5rem 0 0 0",
   },
 });
 
@@ -43,32 +48,28 @@ const Header = (props) => {
   };
 
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
+    <AppBar className={classes.appBar} position="static">
+      {/* <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">Jesse Mazur</Typography>
-        </Toolbar>
+        </Toolbar> */}
+      <Grid container justify="flex-end">
         <Tabs
           value={tabState}
           onChange={handleChange}
           aria-label="header navigation tabs"
         >
-          <Tab
-            value="/"
-            label="Home"
-          />
+          <Grid item>
+          <Tab value="/" label="Home" />
+          </Grid>
           <Tab value="/portfolio" label="Portfolio" />
           <Tab value="/about" label="About" />
         </Tabs>
-      </AppBar>
-
-      <Router>
-        <Route></Route>
-      </Router>
-    </div>
+      </Grid>
+        <div>This is the banner</div>
+    </AppBar>
   );
 };
 
