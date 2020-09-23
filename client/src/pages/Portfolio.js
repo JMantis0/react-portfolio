8a84f08c3c6c1ec7306a80e7c218c3d9a906d95c
JMan <60026263+JMantis0@users.noreply.github.com>
2020-09-23 14:26:32 -0700

    added images to assets folder for portfolio page.  Moved projectData object into the Portfolio folder.  Beginning to use map function to render projects using the projectData object and props.

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -15,55 +15,111 @@ import Project from "../components/Project";
 // Budget-tracker
 // React-Employee Directory]
 
+//I'd like this to cause a modal to pop up when clicked on.  otherwise
+//  has a screenshot with a banner
+// Image
+//  Link?
+const projectData = {
+  javaScriptQuiz: {
+    title: "JavaScript Quiz",
+    deployedLink:
+      "https://jmantis0.github.io/depth-of-knowledge-javascript-edition/",
+    gitHubRepoLink:
+      "https://github.com/JMantis0/depth-of-knowledge-javascript-edition",
+    cardImage: "../assets/javascript-quiz.jpg",
+    description:
+      "Interactive browser based application featuring jQuery DOM manipulation, dynamic CSS, Bootstrap Framework, data-attributes, and local storage.",
+  },
+  weatherDashBoard: {
+    title: "Weather Dashboard",
+    deployedLink: "https://jmantis0.github.io/weather-dashboard/",
+    gitHubRepoLink: "http://github.com/JMantis0/weather-dashboard",
+    cardImage: "../assets/w-dashboard.png",
+    description:
+      "Browser based weather forecast application featuring data called from the OpenWeatherMaps api. ",
+  },
+  workDayScheduler: {
+    title: "Work Day Scheduler",
+    deployedLink: "https://jmantis0.githubio/work-day-scheduler",
+    gitHubRepoLink: "https://github.com/JMantis0/work-day-scheduler",
+    cardImage: "../assets/wd-scheduler.JPG",
+    description:
+      "Task management scheduler application featuring mobile responsive design, dynamic appearance, data storage, css animations, and sound.",
+  },
+  budgetTracker: {
+    title: "Budget Tracker",
+    deployedLink: "https://jbudget-tracker.herokuapp.com/",
+    gitHubRepoLink: "https://github.com/JMantis0/budget-tracker",
+    cardImage: "../assets/budget-tracker.JPG",
+    description: "",
+  },
+};
+
 const Portfolio = () => {
+  console.log("Object.keys(projectData)", Object.keys(projectData));
+  Object.keys(projectData).forEach((key) =>
+    console.log(projectData[key].deployedLink)
+  );
   return (
     <div>
-    <Grid container justify="space-around" spacing={2}>
-      <Grid item xs={4}>
-        <Project />
+      <Grid container justify="space-around" spacing={2}>
+        {Object.keys(projectData).map((key) => {
+          return (
+            <Project
+              projectTitle={projectData[key].title}
+              deployedLink={projectData[key].deployedLink}
+              gitHubRepoLink={projectData[key].gitHubRepoLink}
+              cardImage={projectData[key].cardImage}
+              description={projectData[key].description}
+            />
+          );
+        })}
       </Grid>
-      <Grid item xs={4}>
-        <Project />
+      {/* <Grid container justify="space-around" spacing={2}>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Project />
+      <Grid container justify="space-around" spacing={2}>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
       </Grid>
-    </Grid>
-    <Grid container justify="space-around" spacing={2}>
-      <Grid item xs={4}>
-        <Project />
+      <Grid container justify="space-around" spacing={2}>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-    </Grid>
-    <Grid container justify="space-around" spacing={2}>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-    </Grid>
-    <Grid container justify="space-around" spacing={2}>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-      <Grid item xs={4}>
-        <Project />
-      </Grid>
-    </Grid>
-      </div>
-    
+      <Grid container justify="space-around" spacing={2}>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+        <Grid item xs={4}>
+          <Project />
+        </Grid>
+      </Grid> */}
+    </div>
   );
 };
 
