00f9821941f3ad4b04e39b166b6ce0da975aa973
JMan <60026263+JMantis0@users.noreply.github.com>
2020-10-04 23:02:35 -0700

    Implemented SizeMe library to make PDF on resume responsive.  Seems to be working.

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf";
 import Grid from "@material-ui/core/Grid";
-import {
-  makeStyles
-} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import resume from "../assets/pdf/jesse-mazur-resume.pdf";
 import { pdfjs } from "react-pdf";
+import { SizeMe, withSize } from "react-sizeme";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume(props) {
@@ -22,16 +21,34 @@ function Resume(props) {
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
+  const withSizeHOC = withSize();
 
   return (
     <Grid id="resumeGridContainer" justify="center" container>
-      <Grid item id="resumeGridItem">
-        <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page className={classes.resume} scale={1.4} pageNumber={pageNumber} />
-        </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
+      <Grid item id="resumeGridItem" 
+      style={{ width: "90%" }}
+      >
+        <SizeMe
+          monitorWidth
+          refreshRate={128}
+          refreshMode={"debounce"}
+          render={({ size }) => (
+            <div>
+              {/* <div>My width is {size.width}px</div> */}
+              <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page
+                  width={size.width}
+                  className={classes.resume}
+                  scale={1}
+                  pageNumber={pageNumber}
+                />
+              </Document>
+              <p>
+                Page {pageNumber} of {numPages}
+              </p>
+            </div>
+          )}
+        />
       </Grid>
     </Grid>
   );
