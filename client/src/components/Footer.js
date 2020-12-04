4cdbd7dc05e68847841c59f6134a31ff90d62cfd
JMan <60026263+JMantis0@users.noreply.github.com>
2020-12-04 03:03:58 -0800

    added more padding to footer.  Set position of footerContainer back to static.  Added MetroApp to Portfolio page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -38,28 +38,17 @@ const Footer = ({
       boxShadow: "none",
     },
     bottomNavigation: {
-      // backgroundColor: "#eeeeee",
+      paddingTop: "20px",
     },
     footerContainer: {
-      [mainTheme.breakpoints.up("xs")]: {
-        width: "95%",
-        margin: "0 0 0 0%",
-        overflow: "hidden",
-      },
-      [mainTheme.breakpoints.up("620")]: {
-        width: "90%",
-        margin: "0 0 0 0%",
-      },
-      [mainTheme.breakpoints.up("1300")]: {
-        maxWidth: "1170px",
-        margin: "0 auto 0 auto",
-      },
-
+      // 
+      paddingBottom: "40px",
+      paddingTop: "40px",
       boxShadow:
         "0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)",
       backgroundColor: mainTheme.palette.background.paper,
-      position: "fixed",
-      bottom: "0",
+      // position: "fixed",
+      // bottom: "0",
       "& *": {
         textAlign: "center",
         textTransform: "capitalize",
